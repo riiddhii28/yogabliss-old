6112a997d0da949e45391bd00b1f4b52e5dd58d1
shreyak-29 <shreyakathe2904@gmail.com>
2024-09-30 22:30:02 +0530

    forYou

--- frontend/src/pages/Classes/Classes.jsx
+++ frontend/src/pages/Classes/Classes.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Classes = () => {
+  const[classes, setClasses] = useState([]);
+  useEffect(()=> {
+
+  }, [])
   return (
     <div>Classes</div>
   )
 }
+export default Classes
